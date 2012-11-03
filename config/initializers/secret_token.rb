3e205141e38a879e9c1cb15fcb602c88606436b7
Shaan Shah <shahshaan@gmail.com>
2012-11-02 19:50:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watado::Application.config.secret_token = 'f8b38c5dc1238390aa200f050b4395713569cb0ee7cac778589ac75097bbef06db77872227228976b314792eefff51f9ab23b9d234a42101e3317dbf713b9363'
